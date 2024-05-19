d75282c716e46840659766f63826ebb7651ec8cc
Hazemmohamed123 <132374305+Hazemmohamed123@users.noreply.github.com>
2024-05-20 02:27:46 +0300

    Add files via upload

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,8 @@
--- write a script that prepares a MySQL server for the project
--- create database with the right stuff
+-- Setup for MySQL server
+-- Script to automate setup
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- create user if doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
--- set password for user
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
--- grant privileges to user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- grant select privileges to user on performance_schema database
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- flush privileges
-FLUSH PRIVILEGES;
+USE hbnb_dev_db;
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
